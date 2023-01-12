f232897cde19d90f24674302bb87afb4a789a304
Thomas Zamojski <thomas.zamojski@quadratic-labs.com>
2023-01-12 21:35:22 +0100

    feature: adds Mina's and Starkware's parameters
    Adds parameters for both Mina's Pasta curves pallas and vesta.
    Also adds parameters for Starkware's poseidon for multiple rates.
    
    Includes also a little refactoring. All conversion functions are
    united in the convert.rs module. Poseidon also provide absorb and
    squeeze methods which could be useful for variable-length hashing.

--- src/permutation.rs
+++ src/permutation.rs
@@ -1,239 +1,233 @@
-// use std::str::FromStr;
-use crate::parameters::{load_mds, load_rk, Parameters};
-use ff::*;
+use crate::convert::felts_from_str;
+use crate::parameters::Parameters;
+use ff::PrimeField;
 
-pub struct Poseidon<Fr, const M: usize, const N: usize> {
-    params: &'static Parameters,
-    mds_matrix: [[Fr; M]; M],
-    round_constants: [[Fr; M]; N],
+pub struct Poseidon<'a, GF> {
+    params: &'a Parameters,
+    mds_matrix: Vec<GF>,
+    round_constants: Vec<GF>,
+    offset: usize,
+    state: Vec<GF>,
 }
-impl<Fr, const M: usize, const N: usize> Poseidon<Fr, M, N>
+
+impl<'a, GF> Poseidon<'a, GF>
 where
-    Fr: PrimeField,
+    GF: PrimeField,
 {
     pub fn new(
-        params: &'static Parameters,
-        mds: &[[&'static str; M]; M],
-        rk: &[[&'static str; M]; N],
+        params: &'a Parameters,
     ) -> Self {
         Poseidon {
             params: params,
-            mds_matrix: load_mds(mds),
-            round_constants: load_rk(rk),
+            mds_matrix: felts_from_str(params.mds_matrix),
+            round_constants: felts_from_str(params.round_constants),
+            offset: 0,
+            state: vec![GF::ZERO; params.rate + params.capacity],
         }
     }
 
-    fn ark(&self, state: &mut Vec<Fr>, round: u32) {
-        let r = usize::try_from(round).unwrap();
-        for i in 0..state.len() {
-            state[i].add_assign(&self.round_constants[r][i]);
+    fn ark(&mut self, round: usize) {
+        let size = self.state.len();
+        for i in 0..size {
+            self.state[i].add_assign(&self.round_constants[round*size + i]);
         }
     }
 
-    fn sbox_full(&self, state: &mut Vec<Fr>) {
-        for j in 0..state.len() {
-            let aux = state[j].clone();
+    fn sbox_full(&mut self) {
+        for j in 0..self.state.len() {
+            let aux = self.state[j].clone();
             for _ in 1..self.params.power {
-                state[j].mul_assign(aux);
+                self.state[j].mul_assign(aux);
             }
         }
     }
 
-    fn sbox_partial(&self, state: &mut Vec<Fr>) {
-        let i = state.len() - 1;
-        let aux = state[i].clone();
+    fn sbox_partial(&mut self) {
+        let i = self.state.len() - 1;
+        let aux = self.state[i].clone();
         for _ in 1..self.params.power {
-            state[i].mul_assign(&aux);
+            self.state[i].mul_assign(&aux);
         }
     }
 
-    fn mix(&self, state: &mut Vec<Fr>) {
-        let mut new_state = vec![Fr::ZERO; state.len()];
-        for i in 0..state.len() {
-            for j in 0..state.len() {
-                let mut mij = self.mds_matrix[i][j];
-                mij.mul_assign(&state[j]);
+    fn mix(&mut self) {
+        let size = self.state.len();
+        let mut new_state = vec![GF::ZERO; size];
+        for i in 0..size {
+            for j in 0..size {
+                let mut mij = self.mds_matrix[i*size + j];
+                mij.mul_assign(&self.state[j]);
                 new_state[i].add_assign(&mij);
             }
         }
-        state.clone_from(&new_state);
+        self.state.clone_from(&new_state);
     }
 
-    pub fn permute(&self, state: &mut Vec<Fr>, input: &[Fr]) {
-        // let n_full_rounds = params.n_full_rounds.clone();
-        // let n_partial_rounds = params.n_partial_rounds[t - 2].clone();
-
-        for i in 0..input.len() {
-            state[i].add_assign(input[i]);
-        }
+    fn permute(&mut self) {
         let rf = self.params.n_full_rounds / 2;
         let rp = self.params.n_partial_rounds;
 
         for i in 0..rf {
-            self.ark(state, i);
-            self.sbox_full(state);
-            self.mix(state);
+            self.ark(i);
+            self.sbox_full();
+            self.mix();
         }
         for i in rf..(rf + rp) {
-            self.ark(state, i);
-            self.sbox_partial(state);
-            self.mix(state);
+            self.ark(i);
+            self.sbox_partial();
+            self.mix();
         }
         for i in (rf + rp)..(2 * rf + rp) {
-            self.ark(state, i);
-            self.sbox_full(state);
-            self.mix(state);
+            self.ark(i);
+            self.sbox_full();
+            self.mix();
         }
     }
 
-    pub fn hash(&self, inputs: &[Fr]) -> Result<Vec<Fr>, String> {
-        if inputs.len() == 0 {
-            return Err("Empty inputs".to_string());
+    fn ensure_permuted(&mut self) {
+        // offset should be <= rate, so really testing for equality
+        if self.offset >= self.params.rate {
+            self.permute();
+            self.offset = 0;
         }
-        let n_remaining = inputs.len() % self.params.rate;
-        // Fixed length hash only. How to handle otherwise? Is padding safe?
-        if n_remaining != 0 {
-            return Err(format!(
-                "Input length {} must be a multiple of the hash rate {}",
-                inputs.len(),
-                self.params.rate
-            )
-            .to_string());
-        }
+    }
 
-        let mut state = vec![Fr::ZERO; M];
-        // let rate = usize::try_from(self.params.rate).unwrap();
-        for i in (0..inputs.len()).step_by(self.params.rate) {
-            self.permute(&mut state, &inputs[i..(self.params.rate + i)]);
-        }
-        state.truncate(self.params.output_size);
-        Ok(state)
+    pub fn absorb(&mut self, input: &GF) {
+        self.ensure_permuted();
+        self.state[self.offset].add_assign(input);
+        self.offset += 1;
+    }
+
+    pub fn squeeze(&mut self) -> GF {
+        self.ensure_permuted();
+        let result = self.state[self.offset];
+        self.offset += 1;
+        result
     }
 }
+
+pub fn hash<'a, GF> (
+        inputs: &'a [GF],
+        params: &'a Parameters,
+    ) -> Result<Vec<GF>, String>
+where 
+    GF: PrimeField,
+{
+    if inputs.len() == 0 {
+        return Err("Empty inputs".to_string());
+    }
+    let n_remaining = inputs.len() % params.rate;
+    if n_remaining != 0 {
+        return Err(format!(
+            "Input length {} must be a multiple of the hash rate {}",
+            inputs.len(),
+            params.rate
+        )
+        .to_string());
+    }
+
+    let mut poseidon = Poseidon::<'a, GF>::new(&params);
+    for input in inputs {
+        poseidon.absorb(input);
+    }
+    let mut result = vec![GF::ZERO; params.output_size];
+    for i in 0..params.output_size {
+        result[i] = poseidon.squeeze();
+    }
+    Ok(result)
+}
+
 
 #[cfg(test)]
 mod test_permutation {
     use super::*;
-    use crate::parameters::s128b;
-    use ff::{Field, PrimeField};
-
-    fn get_input() -> Vec<s128b::F253> {
-        let b1: s128b::F253 = s128b::F253::from(7);
-        let b2: s128b::F253 = s128b::F253::from(98);
-        vec![b1.clone(), b2.clone()]
-    }
-
-    fn load_felts<F, const M: usize>(arr: &[&str; M]) -> Vec<F>
-    where
-        F: PrimeField,
-    {
-        let mut result = vec![F::ZERO; M];
-        for i in 0..M {
-            result[i] = F::from_str_vartime(arr[i]).unwrap();
-        }
-        result
-    }
+    use crate::convert::{felts_from_str};
+    use crate::parameters::s128b::{PARAMS, GF};
 
     #[test]
     fn test_ark() {
-        const M: usize = s128b::MDS.len();
-        const N: usize = s128b::RK.len();
-        let hash = Poseidon::<s128b::F253, M, N>::new(&s128b::PARAMS, &s128b::MDS, &s128b::RK);
-        let input = get_input();
-        let mut state = vec![s128b::F253::ZERO; M];
-        for i in 0..input.len() {
-            state[i] = input[i].clone();
-        }
-        hash.ark(&mut state, 0);
+        let mut poseidon = Poseidon::<GF>::new(&PARAMS);
+        let input = vec![GF::from(7), GF::from(98), GF::from(0)];
+        poseidon.state.clone_from(&input);
+        poseidon.ark(0);
         let expected = [
             "10187801339791605336251748402605479409606566396373491958667041943798551150218",
             "8824452141556477327634835943439996420519135454314677708228322513850226510123",
             "5264468709835621148349527988912247104353814123939106227116596276180070073104",
         ];
-        let expected = load_felts::<s128b::F253, M>(&expected);
-        assert_eq!(state, expected);
+        let expected = felts_from_str::<GF>(&expected);
+        assert_eq!(poseidon.state, expected);
     }
 
     #[test]
     fn test_sbox_full() {
-        const M: usize = s128b::MDS.len();
-        const N: usize = s128b::RK.len();
-        let hash = Poseidon::<s128b::F253, M, N>::new(&s128b::PARAMS, &s128b::MDS, &s128b::RK);
+        let mut poseidon = Poseidon::<GF>::new(&PARAMS);
         let state = [
             "10187801339791605336251748402605479409606566396373491958667041943798551150218",
             "8824452141556477327634835943439996420519135454314677708228322513850226510123",
             "5264468709835621148349527988912247104353814123939106227116596276180070073104",
         ];
-        let mut state = load_felts::<s128b::F253, M>(&state);
-        hash.sbox_full(&mut state);
+        poseidon.state.clone_from(&felts_from_str::<GF>(&state));
+        poseidon.sbox_full();
         let expected = [
             "9033127700447853090229678702028773675793347128105171639302548972716183808266",
             "12584005788907507820847858681541330081079761745009746063606627523756483557914",
             "481502024243180892202073663390242313819723218601880119953632240938269076973",
         ];
-        let expected = load_felts::<s128b::F253, M>(&expected);
-        assert_eq!(state, expected);
+        let expected = felts_from_str::<GF>(&expected);
+        assert_eq!(poseidon.state, expected);
     }
 
     #[test]
     fn test_sbox_partial() {
-        const M: usize = s128b::MDS.len();
-        const N: usize = s128b::RK.len();
-        let hash = Poseidon::<s128b::F253, M, N>::new(&s128b::PARAMS, &s128b::MDS, &s128b::RK);
+        let mut poseidon = Poseidon::<GF>::new(&PARAMS);
         let state = [
             "10187801339791605336251748402605479409606566396373491958667041943798551150218",
             "8824452141556477327634835943439996420519135454314677708228322513850226510123",
             "5264468709835621148349527988912247104353814123939106227116596276180070073104",
         ];
-        let mut state = load_felts::<s128b::F253, M>(&state);
-        hash.sbox_partial(&mut state);
+        poseidon.state.clone_from(&felts_from_str::<GF>(&state));
+        poseidon.sbox_partial();
         let expected = [
             "10187801339791605336251748402605479409606566396373491958667041943798551150218",
             "8824452141556477327634835943439996420519135454314677708228322513850226510123",
             "481502024243180892202073663390242313819723218601880119953632240938269076973",
         ];
-        let expected = load_felts::<s128b::F253, M>(&expected);
-        assert_eq!(state, expected);
+        let expected = felts_from_str::<GF>(&expected);
+        assert_eq!(poseidon.state, expected);
     }
 
     #[test]
     fn test_mix() {
-        const M: usize = s128b::MDS.len();
-        const N: usize = s128b::RK.len();
-        let hash = Poseidon::<s128b::F253, M, N>::new(&s128b::PARAMS, &s128b::MDS, &s128b::RK);
+        let mut poseidon = Poseidon::<GF>::new(&PARAMS);
         let state = [
             "9033127700447853090229678702028773675793347128105171639302548972716183808266",
             "12584005788907507820847858681541330081079761745009746063606627523756483557914",
             "481502024243180892202073663390242313819723218601880119953632240938269076973",
         ];
-        let mut state = load_felts::<s128b::F253, M>(&state);
-        hash.mix(&mut state);
+        poseidon.state.clone_from(&felts_from_str::<GF>(&state));
+        poseidon.mix();
         let expected = [
             "13203118710027330771388479782000018409212326464085107030609226142628639238173",
             "5548559894030014093638382051049588462182080648170927883872275099718526588448",
             "13953418491398777380386807509733024828619099654861449366171745749260147070969",
         ];
-        let expected = load_felts::<s128b::F253, M>(&expected);
-        assert_eq!(state, expected);
+        let expected = felts_from_str::<GF>(&expected);
+        assert_eq!(poseidon.state, expected);
     }
 
     #[test]
     fn test_permutation() {
-        const M: usize = s128b::MDS.len();
-        const N: usize = s128b::RK.len();
-        let hash = Poseidon::<s128b::F253, M, N>::new(&s128b::PARAMS, &s128b::MDS, &s128b::RK);
-        let input = get_input();
-        let mut state = vec![s128b::F253::ZERO; M];
-        hash.permute(&mut state, &input);
-        let expected_str = [
+        let mut poseidon = Poseidon::<GF>::new(&PARAMS);
+        let input = vec![GF::from(7), GF::from(98), GF::from(0)];
+        poseidon.state.clone_from(&input);
+        poseidon.permute();
+        let expected = [
             "11053447091811250430558990262025664436943237361628909971717799705027922243051",
             "13019049864140962728034369799908465523605511214062523575537207652022843673387",
             "1746872083923296042231738669780413133264403148587998064386629453972513813613",
         ];
-        let mut expected = vec![s128b::F253::ZERO; M];
-        for i in 0..M {
-            expected[i] = s128b::F253::from_str_vartime(expected_str[i]).unwrap();
-        }
-        assert_eq!(state, expected);
+        let expected = felts_from_str::<GF>(&expected);
+        assert_eq!(poseidon.state, expected);
     }
 }
---
+++ tests/vesta.rs
@@ -0,0 +1,25 @@
+use poseidon::convert::felts_from_str;
+use poseidon::parameters::vesta::GF;
+use poseidon::hash_vesta as hash;
+
+
+#[test]
+fn test_hash_simple() {
+    let input = ["0", "0"];
+    let input = felts_from_str::<GF>(&input);
+    let expected = [
+        "15503507843680028074813146032292970642340699155253454797233283122108694118843",
+        "304291385369895909791985322046133629782080136045317966776007874226152902593"
+    ];
+    let expected = felts_from_str::<GF>(&expected);
+    let output = hash(&input);
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[should_panic]
+fn test_hash_wrong_size() {
+    let input = ["0", "7", "98", "0", "0"];
+    let input = felts_from_str::<GF>(&input);
+    hash(&input);
+}
