a2f2e2b0b34aeb911f7ee8731106c42dcc6885a3
Thomas Zamojski <tzamojski@gaia.quadratic-labs.com>
2023-03-01 17:43:15 +0000

    FIX: formating

--- src/lib.rs
+++ src/lib.rs
@@ -25,17 +25,13 @@
 // Implementation is done for PrimeFields.
 // Question remains of how to handle BinaryFields.
 // Other fields are probably not useful at this point.
-#![cfg_attr(
-    any(target_arch = "wasm32", not(feature = "std")),
-    no_std
-)]
+#![cfg_attr(any(target_arch = "wasm32", not(feature = "std")), no_std)]
 
 #[cfg(feature = "std")]
 include!("./with_std.rs");
 
 #[cfg(not(feature = "std"))]
 include!("./without_std.rs");
-
 
 pub mod fields;
 pub mod permutation;
